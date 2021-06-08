10c6cda5fe8ab692a2a4837cfac7bdcaa73ab354
Kurt Ball <kurtisdball@hotmail.com>
2021-06-08 09:41:05 -0600

    first

---
+++ screens/home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const Home = () => {
+  return (
+    <View>
+      <Text style={styles.container}>Home</Text>
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+  },
+})
+export default Home
