cede632a6874efe45b59c9c789d084ae83e80ae4
Kurt Ball <kurtisdball@hotmail.com>
2021-06-08 18:43:49 -0600

    updated packages.

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -8,12 +8,29 @@ const screens = {
   //Whatever is the first object that will be what is displayed first ie Home
   Home: {
     screen: Home,
+    navigationOptions: {
+      title: 'Game Reviews',
+      headerStyle: {
+        backgroundColor: '#eee',
+      },
+    },
   },
   ReviewDetails: {
     screen: ReviewDetails,
+    navigationOptions: {
+      title: 'Reviews',
+      headerStyle: {
+        backgroundColor: '#eee',
+      },
+    },
   },
 }
 
-const HomeStack = createStackNavigator(screens)
+const HomeStack = createStackNavigator(screens, {
+  defaultNavigationOptions: {
+    headerTintColor: '#444',
+    headerStyle: { backgroundColor: { backgroundColor: '#eee', height: 60 } },
+  },
+})
 
 export default createAppContainer(HomeStack)
